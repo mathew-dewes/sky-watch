d2593d10c8cc6270c9944fd75948a5a47f05444a
Mathew Dewes <mathewdewes01@gmail.com>
2025-09-19 16:41:24 +1200

    Unused code deleted

--- src/app/discussions/_components/Filters.tsx
+++ src/app/discussions/_components/Filters.tsx
@@ -2,24 +2,11 @@
 
 
 import Form from 'next/form'
-import { redirect } from 'next/navigation';
 import DropDown from '@/components/ui/Dropdown';
 
 
 const Communities = ["Northland", "Auckland", "Waikato", "Wellington"]
 
-
-
-export async function filterPosts(formData: FormData){
-  const orderBy = formData.get("orderBy") as string | null;
-  const order = formData.get("order") as string | null;
-    const query = new URLSearchParams();
-  if (orderBy) query.set("orderBy", orderBy);
-  if (order) query.set("order", order);
-
-  redirect(`/post/all?${query.toString()}`);
-  
-}
 
 export default function Filters({community}:{community: string}){
 
