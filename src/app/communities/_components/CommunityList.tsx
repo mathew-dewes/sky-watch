8a80a970aeb4e19359a81da9c6bc0456f7fe6528
Mathew Dewes <mathewdewes01@gmail.com>
2025-09-19 15:05:28 +1200

    Community list added to community page

---
+++ src/app/communities/_components/CommunityList.tsx
@@ -0,0 +1,32 @@
+import Avatar from "@/components/ui/Avatar"
+import Button from "@/components/ui/Button";
+import { getCommunities } from "@/server/queries/community";
+
+import Link from "next/link";
+
+
+
+
+export default async function CommunitiyList(){
+
+        const communtities = await getCommunities();
+    return (
+        <div>
+              {communtities.map((region)=>{
+                            return (
+                                <div className="border-b-2 py-10 w-1/2" key={region.id}>
+                                    <h1>{region.name}</h1>
+                                    <p className="mb-5">Lorem ipsum dolor sit amet.</p>
+                                    <div className="flex items-center gap-2 mb-5">
+                  <p>Moderator -</p>
+                   <Avatar imageUrl={region.moderator.image} name={region.moderator.name}/>
+                                    </div>
+                                   
+                              
+                                    <Link href={`/post/all?community=${region.name}`}><Button text="View posts"/></Link>
+                                </div>
+                            )
+                        })}
+        </div>
+    )
+}
--- src/app/communities/page.tsx
+++ src/app/communities/page.tsx
@@ -1,3 +1,19 @@
-export default function page(){
-    return
+import { authProtection } from "@/server/auth/session"
+import { Suspense } from "react";
+import CommunitiyList from "./_components/CommunityList";
+import LoadingSpinner from "@/components/ui/LoadingSpinner";
+
+export default async function page(){
+    await authProtection();
+
+    return (
+        <div>
+            <h1>Communities:</h1>
+            <Suspense fallback={<LoadingSpinner text="Loading communities..."/>}>
+                <CommunitiyList/>
+            </Suspense>
+
+
+        </div>
+    )
 }
