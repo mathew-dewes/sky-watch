2949f1c99696e7d37404d9836d5844771f040067
Mathew Dewes <mathewdewes01@gmail.com>
2025-09-19 12:03:26 +1200

    Page routes and tailwind styling added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const poppins = Poppins({
+  subsets:["latin"], 
+  weight:["400", "300", "500", "600" ]
 });
 
 export const metadata: Metadata = {
@@ -23,11 +18,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+ <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${poppins.className}  antialiased`}
       >
-        {children}
+
+        <main className="mx-10 sm:mx-20 md:mx-30 lg:mx-50 mt-10">
+      {children}
+        </main>
+  
       </body>
     </html>
   );
