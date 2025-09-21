098f0b959e9b91dc708cefa54bab9506f5fd6d8a
Mathew Dewes <mathewdewes01@gmail.com>
2025-09-21 15:22:13 +1200

    Weather forecast widget improved

--- src/app/(dashboard)/_components/ForcastWidget.tsx
+++ src/app/(dashboard)/_components/ForcastWidget.tsx
@@ -12,22 +12,23 @@ export default async function ForcastWidget({location}:
     return (
         <div className="mt-3">
             <h2>{today}</h2>
-            <div className="mt-5 flex gap-5 flex-wrap">
+            <div className="mt-5 flex justify-between border-t-4">
                 {forcast.list.map((item, key) => {
                     return (
-                        <div key={key} className="px-12 py-4 bg-lightdark-500 rounded-2xl w-fit">
+                        <div key={key} className="px-3 py-4 w-fit border-r-4 border-accent-500">
                             <h2>{epochToLocalShort(item.dt)}</h2>
-                            <div>
+                            <div className="items-center w-fit">
 
-                                <div className="flex items-center">
+                                <div className="flex items-center gap-1 mt-1">
                                     <Image width={30} height={30} alt="Weather icon" src={`https://openweathermap.org/img/wn/${item.weather[0].icon}.png`}></Image>
                                     <p className="mt-1">{item.main.temp.toFixed()} C</p>
                       
                                 </div>
-                                <div className="flex items-center gap-1 mt-1">
+                                {item.rain &&  <div className="flex items-center gap-1 mt-1">
                         <Image width={15} height={15} alt="Rain icon" src={`/drop.png`}></Image>
                               <p className="mt-1">{(item.rain)? item.rain["3h"] : "0.0"} /hr</p>
-                                </div>
+                                </div>}
+                               
   
 
                             </div>
