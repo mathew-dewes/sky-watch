f89abca72055bd9dcba1b07af7a896f3ef80b460
Mathew Dewes <mathewdewes01@gmail.com>
2025-09-22 14:59:15 +1200

    Disable location pin href

--- src/components/ui/LocationPin.tsx
+++ src/components/ui/LocationPin.tsx
@@ -1,14 +1,24 @@
 import Image from "next/image"
+import Link from "next/link"
 
 export default function LocationPin({name}:{
     name: string 
 }){
+       let href = `/discussions?community=${name}`
+    if (name === ""){
+        href = ""
+
+    }
+ 
     return (
-        <div className="flex items-center py-1">
+        
+        <Link  href={href}>
+               <div className="flex items-center py-1">
                         <Image src={"/gps.png"} alt="Location pin" height={30} width={30}></Image>
             <p>{name}</p>
 
    
-        </div>
+        </div></Link>
+ 
     )
 }
